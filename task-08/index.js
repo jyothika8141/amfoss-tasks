56edaa20522734d43bfc57d6e3f30f84aa3da34e
Jyothika <118593670+jyothika8141@users.noreply.github.com>
2022-11-23 21:51:44 +0530

    Add files via upload

---
+++ task-08/index.js
@@ -0,0 +1,34 @@
+function play1() {
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+ }
+
+ function play2() {
+    var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+ }
+
+ function play3() {
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+ }
+
+ function play4() {
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+ }
+
+ function play5() {
+    var audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+ }
+
+ function play6() {
+    var audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+ }
+
+ function play7() {
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+ }
